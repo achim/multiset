23e32ee8f59dcad612ed9e6bb70fbdef2ed24c12
Achim Passen <achim.passen@gmail.com>
2015-03-06 07:51:17 +0100

    bumped dependency versions

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "https://github.com/achim/multiset"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/algo.generic "0.1.1"]]
-  :plugins [[codox "0.6.6"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/algo.generic "0.1.2"]]
+  :plugins [[codox "0.8.10"]])
